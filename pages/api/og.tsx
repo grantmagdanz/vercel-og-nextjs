30d295f0c8e66e0c7b002bead3d2cd56e5b5172a
Grant Magdanz <grant@gm.xyz>
2023-06-20 22:32:06 -0600

    More changes

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -7,23 +7,21 @@ export const config = {
 }
 
 export default async function handler() {
-  const parsedText = parse('<div><p>Here is an image:</p><img src="https://perch-development.s3.amazonaws.com/answer-pictures/Perch___Avatar_07_aac44e90-44c0-4844-a8c4-5e1b09e89374.png"><p>This comes after the image</p></div>')
+  const parsedText = parse('<div><p>Hello, World</p></div>')
 
   const Component = () => {
-    console.log(parsedText)
     if (typeof parsedText === 'string') {
       return <div>{parsedText}</div>
     } else if (Array.isArray(parsedText)) {
       return <div>{parsedText.map((item) => <div>{item}</div>)}</div>
     }
-    console.log("blah")
     return parsedText
   }
 
   return new ImageResponse(
     (
       <div>
-        <p>test</p>
+        <Component />
       </div>
     ),
     {
